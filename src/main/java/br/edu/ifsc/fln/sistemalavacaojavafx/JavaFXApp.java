ff1e3cbf17f1767e369b24cee29dddefc7539cf5
thesimas <luciano.simasjr@gmail.com>
2026-06-16 14:34:00 -0300

    Adicionando Imagens, tanto no logo, como na view do menu

--- src/main/java/br/edu/ifsc/fln/sistemalavacaojavafx/JavaFXApp.java
+++ src/main/java/br/edu/ifsc/fln/sistemalavacaojavafx/JavaFXApp.java
@@ -3,6 +3,7 @@ package br.edu.ifsc.fln.sistemalavacaojavafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import br.edu.ifsc.fln.sistemalavacaojavafx.model.domain.Servico;
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class JavaFXApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(JavaFXApp.class.getResource("/view/FXMLVBoxMainApp.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 650, 630);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/icons/IFSC_logo_vertical.png")));
         stage.setTitle("Sistema Norteador (Lavação) - IFSC Campus Florianópolis");
         scene.getStylesheets().add(getClass().getResource("/css/Style.css").toExternalForm());
         stage.setScene(scene);
